cc60ba08058356cda0878bdad97f268bb7ed843b
theta148 <mihusunny@gmail.com>
2025-09-10 11:17:30 -0400

    Implemented a random picture generator.

--- Lab02_MihirPatel.java
+++ Lab02_MihirPatel.java
@@ -1,18 +1,19 @@
 /*
+ * Click https://github.com/theta148/Lab02_MIhirPatel
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package lab02;
 
-import java.awt.Insets;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.*;
+import java.util.Random;         
 /**
  *
  * @author 6316704
@@ -29,6 +30,12 @@ public class Lab02_MihirPatel extends Application{
     
     @Override
     public void start(Stage primaryStage) {
+        Random random = new Random();
+        int min = 101;
+        int max = 120;
+        
+        int num = random.nextInt(min, max + 1);
+        
         primaryStage.setTitle("Java Games");
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 250, 300);
@@ -37,6 +44,10 @@ public class Lab02_MihirPatel extends Application{
         Label top = new Label("Random Game");
         Label bottom = new Label("Waiting..");
         Label lblimage = new Label();
+        
+        Image img = new Image("file:images/" + num + ".jpg");
+        
+        lblimage.setGraphic(new ImageView(img));
         
         root.setTop(top);
         root.setBottom(bottom);
